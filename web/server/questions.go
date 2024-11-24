0197b6725a24ea43bb1f2cd3e22452b93199adb2
Louis Branch <louis@branch.family>
2024-11-23 22:21:15 -0500

    Fix URL redirect for new questions

--- web/server/questions.go
+++ web/server/questions.go
@@ -217,6 +217,6 @@ func (srv *Server) createQuestion(w http.ResponseWriter, r *http.Request,
 		}
 	}
 
-	uri := fmt.Sprintf("/experiments/%s/assessments/%s/", experiment.PublicID, assessment.PublicID)
+	uri := fmt.Sprintf("/experiments/%s/assessments/%s", experiment.PublicID, assessment.PublicID)
 	http.Redirect(w, r, uri, http.StatusFound)
 }
